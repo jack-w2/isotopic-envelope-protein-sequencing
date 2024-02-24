d182a1d0b18a42ae2b6a3745ab6d85da2921d634
Jacek Wolf <jackw2@tlen.pl>
2024-02-24 16:23:27 +0100

    dictionary implementation of molecular formula class

--- molecular_formula.py
+++ molecular_formula.py
@@ -2,14 +2,32 @@ from masserstein import Spectrum
 
 
 class MolecularFormula:
-    def __init__(self, c, h, n, o):
-        self.c = c
-        self.h = h
-        self.n = n
-        self.o = o
+    def __init__(self, elements_dict):
+        self.c = elements_dict.get('C', 0)
+        self.h = elements_dict.get('H', 0)
+        self.n = elements_dict.get('N', 0)
+        self.o = elements_dict.get('O', 0)
 
     def __add__(self, other):
-        return MolecularFormula(self.c + other.c, self.h + other.h, self.n + other.n, self.o + other.o)
+        updated_elements_dict = {
+            'C': self.c + other.c,
+            'H': self.h + other.h,
+            'N': self.n + other.n,
+            'O': self.o + other.o
+        }
+        return MolecularFormula(updated_elements_dict)
+
+    def __str__(self):
+        s = ''
+        for element, amount in self.__dict__.items():
+            if amount > 0:
+                s += f'{element.upper()}{amount}'
+        return s
 
     def generate_spectrum(self):
-        Spectrum(f'C{self.c}H{self.h}N{self.n}O{self.o}').plot()
+        Spectrum(str(self)).plot()
+
+
+# tests
+formula1 = MolecularFormula({'C': 2, 'H': 6, 'O': 1})
+print(formula1)

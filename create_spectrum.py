749fac84f74560db9fd407c19c4aeba9d9ff8da8
Jacek Wolf <jackw2@tlen.pl>
2024-02-24 16:23:27 +0100

    moved fasta to config file and cleaned up code

--- create_spectrum.py
+++ create_spectrum.py
@@ -9,22 +9,35 @@ def generate_prefixes_and_suffixes(seq):
     return [(seq[:i], seq[i:]) for i in range(1, len(seq))]
 
 
-with open('config.toml', 'rb') as tf:
-    config = tomllib.load(tf)
+def scale_intensities(intensities):
+    intensities_sum = sum(intensities)
+    return [i/intensities_sum for i in intensities]
 
 
-with open('insulin.fasta', 'r') as fasta_file:
-    seq = ''.join(fasta_file.read().splitlines()[1:])
-
-seqs = generate_prefixes_and_suffixes(seq)
-
-envelopes = [IsoTotalProb(0.999, fasta=j) for i in seqs for j in i]
-intensities = []
-for i in config['probs']:
-    intensities.extend([i * 0.5] * 2)
+def load_config_file(config_file_path):
+    with open(config_file_path, 'rb') as tf:
+        config = tomllib.load(tf)
+    return config
 
 
-spectre = IsoDistribution.LinearCombination(envelopes, intensities)
-print([(m, p) for (m, p) in zip(spectre.masses, spectre.probs)])
+def main():
+    config = load_config_file('config.toml')
 
-spectre.plot()
+    seq = config['fasta']
+    seqs = generate_prefixes_and_suffixes(seq)
+
+    envelopes = [IsoTotalProb(0.999, fasta=j) for i in seqs for j in i]
+    intensities = []
+    for i in config['probs']:
+        intensities.extend([i * 0.5] * 2)
+
+    intensities = scale_intensities(intensities)
+
+    spectre = IsoDistribution.LinearCombination(envelopes, intensities)
+    print([(m, p) for (m, p) in zip(spectre.masses, spectre.probs)])
+
+    spectre.plot()
+
+
+if __name__ == '__main__':
+    main()

fdeac6d4358b5280d2a69fe766bf70f144a1e1b8
Jacek Wolf <jackw2@tlen.pl>
2024-02-24 16:18:47 +0100

    Bring back previous implementation - revert "changed molecular fromula class to use Counter from collections instead of usual dictionary"
    This reverts commit 59ebd61ce0ec20656cd1aa1a5292f9d2ada10f9e.

--- molecular_formula.py
+++ molecular_formula.py
@@ -1,17 +1,14 @@
 from masserstein import Spectrum
-from collections import Counter
 
 
 class MolecularFormula:
     def __init__(self, elements_dict):
-        cnt = Counter(elements_dict)
-        self.c = cnt['C']
-        self.h = cnt['H']
-        self.n = cnt['N']
-        self.o = cnt['O']
+        self.c = elements_dict.get('C', 0)
+        self.h = elements_dict.get('H', 0)
+        self.n = elements_dict.get('N', 0)
+        self.o = elements_dict.get('O', 0)
 
     def __add__(self, other):
-        # consider using Counter() + Counter()
         updated_elements_dict = {
             'C': self.c + other.c,
             'H': self.h + other.h,

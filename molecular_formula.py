ae1c7f162990c12a8f9eaa3c403845e35961d86b
Jacek Wolf <jackw2@tlen.pl>
2024-04-03 00:55:33 +0200

    added some class methods

--- molecular_formula.py
+++ molecular_formula.py
@@ -19,6 +19,26 @@ class MolecularFormula:
         }
         return MolecularFormula(updated_elements_dict)
 
+    def __sub__(self, other):
+        updated_elements_dict = {
+            'C': self.c - other.c,
+            'H': self.h - other.h,
+            'N': self.n - other.n,
+            'O': self.o - other.o,
+            'S': self.s - other.s
+        }
+        return MolecularFormula(updated_elements_dict)
+
+    def __eq__(self, other):
+        conditions = [
+            self.c == other.c,
+            self.h == other.h,
+            self.n == other.n,
+            self.o == other.o,
+            self.s == other.s
+        ]
+        return all(conditions)
+
     def __str__(self):
         s = ''
         for element, amount in self.__dict__.items():
@@ -28,8 +48,3 @@ class MolecularFormula:
 
     def generate_spectrum(self):
         Spectrum(str(self)).plot()
-
-
-# tests
-formula1 = MolecularFormula({'C': 2, 'H': 6, 'O': 1})
-print(formula1)

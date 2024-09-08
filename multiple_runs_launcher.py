24ed83388a6a9a37798a47a1a0f3a04a1f378eb6
Jacek Wolf <jackw2@tlen.pl>
2024-09-08 16:09:37 +0200

    added launcher for working with multiple proteins and some logs

---
+++ multiple_runs_launcher.py
@@ -0,0 +1,24 @@
+import os
+import multiprocessing
+from pathlib import Path
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+
+
+def prepare_proteins_list(file_path):
+    with open(file_path, 'r') as handle:
+        return [(protein[0], protein[1].replace('I', 'L')) for protein in SimpleFastaParser(handle)]
+
+def launcher(protein):
+    logfile_name = f'{protein[0].split("|")[1]}.csv'
+    print(logfile_name)
+    Path('tests/logs').mkdir(exist_ok=True)
+    os.system(f'python create_spectrum.py -f {protein[1]} -l tests/logs/{logfile_name}')
+
+def main():
+    proteins = prepare_proteins_list('tests/proteins.fasta')
+    with multiprocessing.Pool() as pool:
+        pool.map(launcher, proteins)
+
+
+if __name__ == "__main__":
+    main()

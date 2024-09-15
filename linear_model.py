fd50020517dea52b1a80ae04be0af28534c7f87d
Jacek Wolf <jackw2@tlen.pl>
2024-09-15 23:28:25 +0200

    added cross validation for linear model and did some code clean up

--- linear_model.py
+++ linear_model.py
@@ -1,7 +1,10 @@
 import csv
+import pandas as pd
+import statsmodels.api as sm
 from pathlib import Path
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
 
 logs = list(Path('tests/logs').glob('*'))
 
@@ -20,19 +23,76 @@ print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-#reg = linear_model.LinearRegression()
-#import numpy as np
-#reg.fit(np.array(x_train, dtype=np.float64), y_train)
-
-#print(reg.coef_)
-#print(reg)
-import statsmodels.api as sm
-import pandas as pd
-
 x_train = pd.DataFrame(x_train)
 y_train = pd.Series(y_train)
-sm.add_constant(x_train)
-mod = sm.OLS(y_train, x_train)
-res = mod.fit()
-print(res.summary())
 
+
+class MyLinearRegression(BaseEstimator, RegressorMixin):
+    def __init__(self, fit_intercept=True):
+
+        self.fit_intercept = fit_intercept
+
+
+    """
+    Parameters
+    ------------
+    column_names: list
+            It is an optional value, such that this class knows 
+            what is the name of the feature to associate to 
+            each column of X. This is useful if you use the method
+            summary(), so that it can show the feature name for each
+            coefficient
+    """
+    def fit(self, X, y, column_names=None):
+
+        if column_names is None:
+            column_names = []
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+
+
+        self.X_ = X
+        self.y_ = y
+
+        if len(column_names) != 0:
+            X = pd.DataFrame(X)
+            column_names.insert(0,'intercept')
+            print('X ', X)
+            X.columns = column_names
+
+        self.model_ = sm.OLS(y, X)
+        self.results_ = self.model_.fit()
+        return self
+
+
+    def predict(self, X):
+        # Check is fit had been called
+        check_is_fitted(self, 'model_')
+
+        # Input validation
+        X = check_array(X)
+
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+
+    def get_params(self, deep = False):
+        return {'fit_intercept':self.fit_intercept}
+
+
+    def summary(self):
+        print(self.results_.summary())
+
+
+model = MyLinearRegression()
+model.fit(x_train, y_train)
+model.summary()
+model.predict(x_train)
+
+scores = cross_val_score(MyLinearRegression(), x_train, y_train, cv=10, scoring='neg_mean_squared_error', n_jobs=-1)
+print(scores)
+print(scores.mean())
